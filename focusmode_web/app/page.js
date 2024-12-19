8cb08c5bc58eb0a48b22d49afbf7a8123b9df666
auspy <vinayak.kshetez0102@gmail.com>
2024-12-19 13:30:16 +0530

    refactor: remove testimonial section

--- focusmode_web/app/page.js
+++ focusmode_web/app/page.js
@@ -16,7 +16,7 @@ export default function Page() {
       <Header />
       <Hero />
       <FeatureList />
-      <TestimonialSection />
+      {/* <TestimonialSection /> */}
       <CTASection />
       <SiteFooter />
     </>
